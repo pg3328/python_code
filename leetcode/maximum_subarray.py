ca510cf179f06e5cb73be19f4945ba73caece8d0
pg3328 <pg3328@rit.edu>
2023-01-24 16:56:19 -0500

    updated version

--- leetcode/maximum_subarray.py
+++ leetcode/maximum_subarray.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        output_sum = float("-inf")
-        local_max = float("-inf")
-        for i in range(0,len(nums)):
-            local_max =nums[i]
-            local_sum = local_max
-            for j in range(i+1,len(nums)):
-                local_sum = local_sum+nums[j]
-                local_max = max(local_sum,local_max)
-            output_sum = max(local_max,output_sum)
+        opt=[]
+        opt.append(nums[0])
+        maximum = float("-inf")
+        for i in range(1,len(nums)):
+            opt.append(max(opt[i-1]+nums[i], nums[i]))
+            if maximum < opt[len(opt)-1]:
+                maximum = opt[len(opt)-1]
+        return maximum
+
